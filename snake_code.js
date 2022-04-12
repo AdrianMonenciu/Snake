c01d5024cc43f369aa228aa27d7ea76696bd72a0
AdrianMonenciu <101457701+AdrianMonenciu@users.noreply.github.com>
2022-04-12 22:32:35 +0100

    Rev_2

--- snake_code.js
+++ snake_code.js
@@ -1,102 +1,106 @@
-var SIZE = 500; // Size of the field
-var GRID_SIZE = SIZE / 50;
-var field = document.getElementById('field');
-field.style.width = field.style.height = SIZE + 'px';
-var ctx = field.getContext('2d');
-ctx.scale(2, 2); // Scale the canvas
+const gridSize = 500; // Size of the field
+const tileSize = 25; // Size of the tiles
+let field = document.getElementById('field');
+let ctx = field.getContext('2d');
   
 var direction = newDirection = 1; // -2: up, 2: down, -1: left, 1: right
 var snakeLength = 1;
-var snake = [{x: SIZE / 2, y: SIZE / 2}]; // Snake starts in the center
-var food = null;
-var end = false;
+var snake = [{x: gridSize / 2, y: gridSize / 2}]; // Snake starts in the center
+var food = {x: randomFood(), y: randomFood()};
 var score = 0;
 
 function updateSnakeGame() {
-  var newSnakeHead = {x: snake[0].x, y: snake[0].y};
-  if (Math.abs(direction) !== Math.abs(newDirection)) { // Change directon if the new direction is a new axis
-    direction = newDirection;
-  }
-  var axis = Math.abs(direction) === 1 ? 'x' : 'y'; // 1, -1 are X; 2, -2 are Y
-  if (direction < 0) {
-    newSnakeHead[axis] -= GRID_SIZE; // Move left or down
-  } else {
-    newSnakeHead[axis] += GRID_SIZE; // Move right or up
-  }
-  if (food && food.x === newSnakeHead.x && food.y === newSnakeHead.y) { // Detect if the head is in the same cell as the food
-    food = null;
-    snakeLength += 2;
-    score++;
-  }
-  if (end) {
-    handleGameEnd();
+  updateSnakeHead();
+  eatFoodCheck();
+  updateSnakeTail();
+  if (collisionCheck()) {
+    gameEndUpdate();
     if(newDirection == 5){
     location.reload(); 
     }
   } else {
-    handleFieldReset();
-    handleScoreUpdate();
-    handleDrawSnakeAndFood();
-    snake.unshift(newSnakeHead); // Add a new head to front
-    snake = snake.slice(0, snakeLength);
-    if (newSnakeHead.x < 0 || newSnakeHead.x >= SIZE || newSnakeHead.y < 0 || newSnakeHead.y >= SIZE) { // Detect wall collisions
-      end = true;
-    }
-    handleTtailCollisionCheck(newSnakeHead);
+    gameFieldReset();
+    scoreUpdate();
+    gameFieldUpdate();
   }
+}
+
+function updateSnakeHead() {
+  let newSnakeHead = {x: snake[0].x, y: snake[0].y};
+  if (Math.abs(direction) !== Math.abs(newDirection)) { // Change directon if the new direction is a new axis
+    direction = newDirection;
+  }
+  let axis = Math.abs(direction) === 1 ? 'x' : 'y'; // 1, -1 are X; 2, -2 are Y
+  if (direction < 0) {
+    newSnakeHead[axis] -= tileSize; // Move left or down
+  } else {
+    newSnakeHead[axis] += tileSize; // Move right or up
+  }
+  snake.unshift(newSnakeHead); // Add a new head to front
+}
+
+function eatFoodCheck() {
+  if (food.x === snake[0].x && food.y === snake[0].y) { // Detect if the head is in the same cell as the food
+    snakeLength += 1;
+    score++;
+    let newFoodOk = false;
+    while (newFoodOk === false) {
+      newFoodOk = true;
+      food = {x: randomFood(), y: randomFood()};
+      for (let i = 0; i < snake.length; i++) {
+        if (snake[i].x === food.x && snake[i].y === food.y) newFoodOk = false;
+      }
+    }
+  }
+}
+
+function updateSnakeTail() {
+  snake = snake.slice(0, snakeLength);
+}
+
+function collisionCheck() {
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) return true;  // Detect head with tail collision
+  }
+  if (snake[0].x < 0 || snake[0].x >= gridSize || snake[0].y < 0 || snake[0].y >= gridSize) return true; // Detect wall collisions
+  return false;
 }
 
 function randomFood() {
-  return Math.floor(Math.random() * SIZE / GRID_SIZE) * GRID_SIZE;
+  return Math.floor(Math.random() * gridSize / tileSize) * tileSize;
 }
 
-function coordToString(obj) {
-  return [obj.x, obj.y].join(',');
-}
-
-function handleFieldReset() {
+function gameFieldReset() {
   ctx.fillStyle = '#22424a';
-  ctx.fillRect(0, 0, SIZE, SIZE); // Reset the field
+  ctx.fillRect(0, 0, gridSize, gridSize); // Reset the field
 }
 
-function handleGameEnd() {
+function gameEndUpdate() {
   ctx.fillStyle = '#e8dbb0';
   ctx.font = '30px Monospace';
   ctx.textAlign = 'center';
-  ctx.fillText('Game Over - Score: '+score, SIZE / 2, SIZE / 2);
-  ctx.fillText('SPACE to continue', SIZE / 2, 300);
+  ctx.fillText('Game Over - Score: '+score, gridSize / 2, gridSize / 2);
+  ctx.fillText('SPACE to continue', gridSize / 2, 300);
 }
 
-function handleScoreUpdate() {
+function scoreUpdate() {
   document.getElementById('score').innerHTML = score;
 }
 
-function handleTtailCollisionCheck(newSnakeHead) {
-  var snakeObj = {};
-  for (var i = 0; i < snake.length; i++) {
-    var a = snake[i];
-    if (i > 0) snakeObj[coordToString(a)] = true; // Build a collision lookup object
-  }
-  if (snakeObj[coordToString(newSnakeHead)]) end = true; // Collided with the snake tail
-}
-
-function handleDrawSnakeAndFood() {
+function gameFieldUpdate() {
   ctx.fillStyle = '#15b31b';
-  var snakeObj = {};
   for (var i = 0; i < snake.length; i++) {
     var a = snake[i];
-    ctx.fillRect(a.x, a.y, GRID_SIZE-1, GRID_SIZE-1); // Paint the snake
-  }
-  while (!food || snakeObj[coordToString(food)]) { // Place a food if needed
-    food = {x: randomFood(), y: randomFood()};
+    ctx.fillRect(snake[i].x, snake[i].y, tileSize-1, tileSize-1); // Paint the snake
   }
   ctx.fillStyle = '#f2d729';
-  ctx.fillRect(food.x, food.y, GRID_SIZE, GRID_SIZE); // Paint the food
+  ctx.fillRect(food.x, food.y, tileSize, tileSize); // Paint the food
 }
 
 window.onload = function() {
-  setInterval(updateSnakeGame, 100); // Start the game loop
+  setInterval(updateSnakeGame, 150); // Start the game loop
   window.onkeydown = function(e) {
-    newDirection = {37: -1, 38: -2, 39: 1, 40: 2, 32: 5}[e.keyCode] || newDirection; //32 = 5 for space restart
+    newDirection = {37: -1, 38: -2, 39: 1, 40: 2, 32: 5}[e.keyCode] || newDirection; 
+    // -2: up Key, 2: down Key, -1: left Key, 1: right Key, 5: Space Key - Restart
   };
 };
